b40ee37225de7923b79e89e1da488e5ebbcbc4cf
Rsubhamsharma <rssubham63@gmail.com>
2025-07-21 20:53:29 +0530

    comment change

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,6 +1,9 @@
-//This file is used  to handle API errors in a consistent way across the application. It extends the built-in Error class to create a custom error type that includes a status code and a message. This allows for better error handling and response formatting in API endpoints.
+//This file is used  to handle API errors in a consistent way across the application. 
+// It extends the built-in Error class to create a custom error type that includes a status code and a message. 
+// This allows for better error handling and response formatting in API endpoints.
 //This is writen to avaoid repetitive code in the application and to provide a consistent error response format.
-//In the app we can get api errors so to avoid the errors we write some code to handle the errors in a consistent way across the app
+//In the app we can get api errors so to avoid the errors we write some code to 
+// handle the errors in a consistent way across the app
 //so to avoid the repetitive code we write this file to handle the errors in a consistent way across the app
 class ApiError extends Error {
     constructor(
